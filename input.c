edab2654355528d60d6f8527466be352e48f14a3
Riya Bisht <79960426+b1shtream@users.noreply.github.com>
2025-08-29 20:30:37 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//single line c
+/*
+multiple
+line
+co
+*/
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum: %d\n", a + b);
+    return 0;
+}
